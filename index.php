0fd43cf0daea5a8503c5130d77023852d0c8336c
TheMor <the.mor.ua@gmail.com>
2020-02-12 12:23:13 +0200

    0.0.2

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
 require_once 'function.php';
 
-$pattern = '(foo)(bar)(baz)';
-$string = 'foobarbaz';
+$pattern = '^([a-z0-9]+)';
+$pattern = '#'.$pattern.'#';
+$pattern = $pattern.'i';
+
+$string = 'fooBar756765baZ';
 
 preg_match(
-    '#'.$pattern.'#',
+    $pattern,
     $string,
     $matches
 //    ,PREG_OFFSET_CAPTURE
