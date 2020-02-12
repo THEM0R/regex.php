1292f8596d7affa3dccb7bd00be8b9be7407e959
TheMor <the.mor.ua@gmail.com>
2020-02-12 12:59:37 +0200

    0.0.3 lesson 1

--- index.php
+++ index.php
@@ -1,17 +1,22 @@
 <?php
 require_once 'function.php';
 
-$pattern = '^([a-z0-9]+)';
-$pattern = '#'.$pattern.'#';
-$pattern = $pattern.'i';
+// lesson 0
+//
+//$pattern = '^([a-z0-9]+)';
+//$pattern = '#'.$pattern.'#';
+//$pattern = $pattern.'i';
+//
+//$string = 'fooBar756765baZ';
+//
+//preg_match(
+//    $pattern,
+//    $string,
+//    $matches
+////    ,PREG_OFFSET_CAPTURE
+//);
+//
+//pr($matches);
 
-$string = 'fooBar756765baZ';
 
-preg_match(
-    $pattern,
-    $string,
-    $matches
-//    ,PREG_OFFSET_CAPTURE
-);
-
-pr($matches);
+// lesson 1
