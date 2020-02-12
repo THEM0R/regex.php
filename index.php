ed1213c798a5ad636ccf545530c1187b3757f6de
TheMor <the.mor.ua@gmail.com>
2020-02-12 13:15:18 +0200

    0.0.3 lesson 2

--- index.php
+++ index.php
@@ -18,5 +18,31 @@ require_once 'function.php';
 //
 //pr($matches);
 
+//echo '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, ipsum, veniam. Commodi consectetur dolorem, fugit iure perspiciatis ratione tenetur! Alias delectus dolore pariatur porro ratione suscipit unde veritatis? Ea, perspiciatis.</p>';
 
 // lesson 1
+
+// #\[hide].*?\[/hide]#gi
+
+//Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, ipsum, veniam. Commodi consectetur dolorem, fugit
+//[HIDE]SLAVIK[/HIDE]
+//iure perspiciatis ratione tenetur! Alias delectus dolore pariatur porro ratione suscipit unde veritatis? Ea, perspiciatis.
+
+// replace : links
+
+
+
+// lesson 2
+
+
+
+
+
+
+
+
+
+
+
+
+
